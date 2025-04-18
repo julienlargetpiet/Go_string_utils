83a4fb81dc9cd3735b65cbc5ef6f6c9fa8327a26
Julien <pgyygp090@gmail.com>
2025-04-18 11:58:40 +0200

    added imports

--- utils.go
+++ utils.go
@@ -1,5 +1,11 @@
 package Go_string_utils
 
+import (
+  "fmt"
+  "os"
+  "bufio"
+)
+
 func Int32ToString(x *int32) string {
   const base int32 = 10
   var remainder int32
@@ -75,4 +81,41 @@ func fast_diff(file1 *string, file2 *string, sep *string) {
   }
 }
 
+func fast_diff2(file1 *string, file2 *string, sep *string) string {
+  var dataa string
+  var datab string
+  var comp bool = true
+  var rtn_str string = ""
+  filea, err := os.Open(*file1) 
+  if err != nil {
+    return
+  }
+  defer filea.Close()
+  scannera := bufio.NewScanner(filea)
+  fileb, err := os.Open(*file2) 
+  if err != nil {
+    return
+  }
+  defer fileb.Close()
+  scannerb := bufio.NewScanner(fileb)
+  for scannerb.Scan() && comp {
+    datab = scannerb.Text()
+    scannera.Scan()
+    dataa = scannera.Text()
+    for datab != dataa && scannera.Scan() {
+      fmt.Printf("> %v%v \n", dataa, *sep)
+      dataa = scannera.Text()
+    }
+    comp = (datab == dataa)
+    if comp {
+      fmt.Printf("%v%v%v\n", dataa, *sep, datab)
+    } else {
+      fmt.Printf("%v> %v\n", *sep, datab)
+    }
+  }
+  for scannerb.Scan() {
+    datab = scannerb.Text()
+    fmt.Printf("%v> %v\n", *sep, datab)
+  }
+}
 
